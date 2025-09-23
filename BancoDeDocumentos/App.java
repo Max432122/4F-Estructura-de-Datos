ec36c82fd418d8ebd33867a3c904fb472a8f765b
Max432122 <20243ds175@utez.edu.mx>
2025-09-22 18:17:11 -0600

    agregue el banco de documentos

---
+++ BancoDeDocumentos/App.java
@@ -0,0 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Queue<Client> queue = new ArrayDeque<>();
+
+        Client c1 = new Client(1, "Juanito");
+        c1.addDocument("Doc 1 de Juanito");
+        c1.addDocument("Doc 2 de Juanito");
+
+        Client c2 = new Client(2, "Juanita");
+        c2.addDocument("Doc 1 de Juanita");
+        c2.addDocument("Doc 2 de Juanita");
+
+        Client c3 = new Client(3, "Pedro");
+        c3.addDocument("Doc 1 de Pedro");
+        c3.addDocument("Doc 2 de Pedro");
+
+        queue.offer(c1);
+        queue.offer(c2);
+        queue.offer(c3);
+
+        System.out.println("Cola inicial: " + queue);
+
+        Client clienteAtendido = queue.poll();
+        if (clienteAtendido != null) {
+            System.out.println("Cliente en atencion: " + clienteAtendido.name);
+            clienteAtendido.attend();
+        }
+
+        System.out.println("Cola restante: " + queue);
+    }
+}
---
+++ BancoDeDocumentos/Client.java
@@ -0,0 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Client {
+    int id;
+    String name;
+    Deque<String> docs;
+
+    Client(int id, String name) {
+        this.id = id;
+        this.name = name;
+        docs = new ArrayDeque<>();
+    }
+
+    public void addDocument(String newDoc) {
+        docs.push(newDoc);
+    }
+
+    public void attend() {
+        while (!docs.isEmpty()) {
+            System.out.println("Documento retirado: " + docs.pop());
+        }
+    }
+}

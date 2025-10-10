075d003dfd2bacdb518958af1e6368cca2effb49
CC7 <20243ds185@utez.edu.mx>
2025-10-10 08:58:41 -0600

    Agregue esto

---
+++ sumaDeArreglo/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+          int[][] arrTest={
+                {1,3,7,8},
+                {0,6,3,1},
+                {1,2,3,4},
+                {0,5,3,0},
+                {2,5,9,0}
+        };
+
+
+        int sumaTotal = 0;
+
+        for (int i = 0; i < arrTest.length; i++) {
+            for (int j = 0; j < arrTest[i].length; j++) {
+                sumaTotal += arrTest[i][j];
+            }
+        }
+        System.out.println(sumaTotal);
+    
+    }
+}
